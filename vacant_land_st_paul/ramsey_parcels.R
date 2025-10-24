acbfa8584e317051b75f02e0e7d44fdee69aa053
MatthewHockert <matthew.hockert@gmail.com>
2025-10-24 10:11:10 -0500

    commets added

--- vacant_land_st_paul/ramsey_parcels.R
+++ vacant_land_st_paul/ramsey_parcels.R
@@ -12,7 +12,7 @@ ramsey_data_24 <- st_read("shp_plan_regonal_parcels_2024",layer="Parcels2024Rams
 names(ramsey_data_24)
 st_paul_24 <- subset(ramsey_data_24, CTU_NAME %in% c("Saint Paul"))
 
-
+#identify vacant parcels in St. Paul ----
 st_paul_24 <- st_paul_24 %>%
   mutate(
     vacant_strict = ifelse(
@@ -29,6 +29,8 @@ st_paul_24 <- st_paul_24 %>%
       "Other Parcels"
     )
   )
+
+# plot them
 sum(st_paul_24$vacant_strict,na.rm=T)
 st_paul_24_v <- subset(st_paul_24,vacant_strict==1)
 ggplot(st_paul_24) +
@@ -49,7 +51,6 @@ ggplot(st_paul_24) +
 ggplot(st_paul_24) +
   geom_sf(aes(fill = factor(vacant_strict))) +
   scale_fill_manual(values = c("0" = "grey80", "1" = "red"))
-
 
 vac <- st_paul_24 %>% filter(vacant_strict == 1 & !is.na(USECLASS1))
 vac_ll <- sf::st_transform(vac, 4326)
@@ -75,10 +76,7 @@ leaflet(options = leafletOptions(zoomControl = TRUE)) %>%
   ) %>%
   addLegend(pal = pal, values = vac_ll$USECLASS1, title = "USECLASS1", opacity = 0.7)
 
-stpaul_revenue <- st_paul_24 %>%
-  st_drop_geometry()%>%
-  dplyr::summarise(total_revenue = sum(TOTAL_TAX, na.rm = TRUE))
-
+# Crude Calculatation of taxes ----
 stpaul_summary <- st_paul_24 %>%
   st_drop_geometry()%>%
   summarise(
@@ -88,16 +86,59 @@ stpaul_summary <- st_paul_24 %>%
 
 stpaul_summary <- stpaul_summary %>%
   mutate(tax_rate = total_tax_capacity  / levy)
+# Didn't match observed rate - could be do to many things
 
+# New tax calculation ----
 st_paul_24$city_rate <- .497
-st_paul_24$city_tax_est <- st_paul_24$TAX_CAPAC*st_paul_24$city_rate
+st_paul_24$city_tax_est <- st_paul_24$TAX_CAPAC*st_paul_24$city_rate # what does each parcel pay to the city?
 sum(st_paul_24$city_tax_est,na.rm = T)
 unique(st_paul_24$DWELL_TYPE[st_paul_24$vacant == 1])
-
+st_paul_24$tax_acre <- st_paul_24$city_tax_est/st_paul_24$ACRES_POLY
+hist(st_paul_24$tax_acre)
 # st_paul_24 <- st_paul_24 %>%
 #   mutate(
 #     parcel_type = ifelse(vacant == 1, DWELL_TYPE, "Other Parcels")
 #   )
+st_paul_24 %>%
+  st_drop_geometry()%>%
+  filter(tax_acre > 0) %>%
+  group_by(vacant_strict)%>%
+  summarize(avg_tax_acre = mean(tax_acre,na.rm=T))
+
+st_paul_24 <- st_paul_24 %>%
+  mutate(super_class = case_when(
+    # --- Vacant (priority) ---
+    grepl("VACANT|UNIMPROVED", USECLASS1, ignore.case = TRUE) ~ "Vacant",
+    
+    # --- Single-Family Residential ---
+    grepl("SINGLE UNIT", USECLASS1, ignore.case = TRUE) ~ "Single-Family Residential",
+    
+    # --- Multi-Family Residential ---
+    grepl("1-3 UNITS|4 OR MORE UNITS|APARTMENT|HOUSING|4D", USECLASS1, ignore.case = TRUE) ~ "Multi-Family Residential",
+    
+    # --- Commercial ---
+    grepl("COMMERCIAL", USECLASS1, ignore.case = TRUE) ~ "Commercial",
+    
+    # --- Manufacturing / Industrial ---
+    grepl("INDUSTRIAL|MANUFACTURING", USECLASS1, ignore.case = TRUE) ~ "Manufacturing / Industrial",
+    
+    # --- Agricultural ---
+    grepl("AGRICULTURAL", USECLASS1, ignore.case = TRUE) ~ "Agricultural",
+    
+    # --- Exempt / Institutional / Government ---
+    grepl("5E|EXEMPT|CHURCH|SCHOOL|COLLEGE|HOSPITAL|MUNICIPAL|STATE|COUNTY|PUBLIC|FEDERAL|CHARITABLE|CEMETERY|PILT",
+          USECLASS1, ignore.case = TRUE) ~ "Exempt / Institutional / Government",
+    
+    # --- Catch-all ---
+    TRUE ~ "Other / Unknown"
+  ))
+st_paul_24 %>%
+  st_drop_geometry()%>%
+  filter(tax_acre > 0) %>%
+  group_by(super_class)%>%
+  summarize(avg_tax_acre = mean(tax_acre,na.rm=T),
+            avg_tax = mean(city_tax_est,na.rm = T))%>%
+  arrange(desc(avg_tax_acre))
 
 ggplot(st_paul_24) +
   geom_sf(aes(fill = parcel_type), color = NA) +
@@ -125,19 +166,30 @@ ggplot() +
   ) +
   theme_minimal()
 
+# Verify that data has a vacant flag 
 flag_col <- if ("vacant_strict" %in% names(st_paul_24)) "vacant_strict" else stop("No vacant flag found")
 
+# make sure tax rate and dollars are in data
 st_paul_24 <- st_paul_24 %>%
   mutate(city_rate = if (!"city_rate" %in% names(st_paul_24)) 0.497 else city_rate,
          city_tax_est = TAX_CAPAC * city_rate)
 
+# find the total revenue from property taxes
 city_levy_est <- st_paul_24 %>% st_drop_geometry() %>% summarise(v = sum(city_tax_est, na.rm = TRUE)) %>% pull(v)
-shortfall <- 0.053 * city_levy_est
 
-n_vacant <- st_paul_24 %>% st_drop_geometry() %>% filter(.data[[flag_col]] == 1) %>% summarise(n = n()) %>% pull(n)
+#simulate 2024 shortfall
+shortfall <- 0.053 * city_levy_est
+shortfall
+
+n_vacant <- sum(st_paul_24$vacant_strict, na.rm = TRUE)
+n_vacant
+
 current_fee <- 2705
 required_fee_full <- if (n_vacant > 0) shortfall / n_vacant else NA_real_
+required_fee_full
+
 multiplier_full <- required_fee_full / current_fee
+multiplier_full
 
 sensitivity <- tibble(
   compliance = seq(0.5, 1.0, by = 0.05)
